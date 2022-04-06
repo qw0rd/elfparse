c4c6c1ea82bef6210a26d251cd431b87ca2c0ead
David John <dave@qword.space>
2022-04-06 17:50:50 +0530

    fix example

--- examples/parse.rs
+++ examples/parse.rs
@@ -9,32 +9,5 @@ fn main() {
     println!("{:?}", file.header);
 
     let mut sections = file.sections();
-
-    println!("{}", sections.count());
-
-/*
-    let section_table = file.sections().nth(file.header.e_shstrndx as usize).unwrap();
-    println!("{:?}", section_table);
-
-    let mut curr = section_table.sh_offset as usize;
-    let mut from = section_table.sh_offset as usize;
-    let to = (from + section_table.sh_size as usize) as usize;
-
-/*
-    while curr < to {
-        if data[curr] == 0 {
-            let str_maybe = &data[from..curr];
-            let name = core::str::from_utf8(str_maybe).unwrap();
-            println!("{}", name);
-            from = curr + 1;
-        } 
-        curr += 1;
-    }
-    */
-    */
-
-    for section in file.sections() {
-        println!("{:?}", file.section_name(&section));
-        println!("{:x?}", section);
-    }
+    let text_sec = file.lookup_section(".text");
 }
